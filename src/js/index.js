a8f603f2a3036b72b5fbc511b9368acefb8abbde
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-04-16 18:11:39 +0300

    adding src and libraries

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../css/styles.css';
+import Notiflix from 'notiflix';
+const axios = require('axios').default;
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const Handlebars = require('handlebars');
